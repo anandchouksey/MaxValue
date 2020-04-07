6e657b8d2afd4e780397168491105276a65e8b36
Anand <anandchouksey@rocketmail.com>
2020-04-07 17:32:31 +0200

    configuration added

---
+++ src/main/java/maxvalue/config/MaxValueConfiguration.java
@@ -0,0 +1,9 @@
+package maxvalue.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("maxValuing.*")
+public class MaxValueConfiguration {
+}
